f517d5c3f1fbdd6eaa6697ceb28ef10535705dff
AdamDoquiatan <adamdoq@gmail.com>
2018-12-25 11:58:03 -0800

    added List View Demo

---
+++ ListViewDemo/app/src/main/java/com/adamdoq/listviewdemo/MainActivity.java
@@ -0,0 +1,55 @@
+package com.adamdoq.listviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ListView myListView = findViewById(R.id.myListView);
+
+        final ArrayList<String> friends = new ArrayList<>(
+                Arrays.asList("Friend", "Friendo", "Friendorino")
+        );
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, friends);
+
+        myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Hello " + friends.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+
+
+
+
+
+    }
+}
