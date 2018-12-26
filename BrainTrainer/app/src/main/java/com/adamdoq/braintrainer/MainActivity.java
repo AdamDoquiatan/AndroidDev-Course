bcd4545ddb7a79f72c0c982facc8b79d66015dba
AdamDoquiatan <adamdoq@gmail.com>
2018-12-25 21:10:56 -0800

    added Brain Trainer + layout

---
+++ BrainTrainer/app/src/main/java/com/adamdoq/braintrainer/MainActivity.java
@@ -0,0 +1,23 @@
+package com.adamdoq.braintrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void select(View view) {
+
+    }
+
+
+    public void replay(View view)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
